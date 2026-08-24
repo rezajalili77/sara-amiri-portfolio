1f004877d3280ef32ecd53b8538ca4a6d8b3c888
rezajalili77 <jalili.reza@hotmail.com>
2026-08-24 15:00:48 +0200

    Add files via upload
    remaining

---
+++ app/Http/Controllers/RoutingController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RoutingController extends Controller
+{
+    public function preview()
+    {
+        return view('preview');
+    }
+    public function root()
+    {
+        return view('index');
+    }
+    /**
+     * first level route
+     */
+    public function firstLevel(Request $request, $first)
+    {
+        return view($first);
+    }
+
+    /**
+     * second level route
+     */
+    public function secondLevel(Request $request, $first, $second)
+    {
+        return view($first . '.' . $second);
+    }
+
+    /**
+     * third level route
+     */
+    public function thirdLevel(Request $request, $first, $second, $third)
+    {
+        return view($first . '.' . $second . '.' . $third);
+    }
+}
